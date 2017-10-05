e91bb249013822c2f99aae0c09f82acc54dbb7d4
RR <rai@bcn.ninja>
2017-10-05 20:56:31 +0200

    Initial commit

---
+++ app/src/main/java/org/escoladeltreball/rranguera/m10_test2_github/MainActivity.java
@@ -0,0 +1,13 @@
+package org.escoladeltreball.rranguera.m10_test2_github;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
